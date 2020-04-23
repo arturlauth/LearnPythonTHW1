7e4a950b11d30d146517c4755477967407000d3e
arturlauth-dev <arturlauthmain@gmail.com>
2020-04-23 20:11:12 -0300

    removing

--- projects/Project_1/setup.py
+++ projects/Project_1/setup.py
@@ -5,15 +5,15 @@ except ImportError:
 
 config = {
     'description': 'My Project',
-    'author': 'My Name',
-    'url': 'URL to get it at.',
+    'author': 'Artur Henrique Lauth',
+    'url': 'link do git',
     'download_url': 'Where to download it.',
-    'author_email': 'My email.',
+    'author_email': 'arturlauthmain@gmail.com',
     'version': '0.1',
     'install_requires': ['nose'],
     'packages': ['NAME'],
     'scripts': [],
-    'name': 'projectname'
+    'name': 'Project_1'
 }
 
 setup(**config)

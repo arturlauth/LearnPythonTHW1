1dcefb50c14c3466a14efb93514c495d36ca2d94
arturlauth-dev <arturlauthmain@gmail.com>
2020-04-23 16:54:01 -0300

    fix flake8

--- oop_test.py
+++ oop_test.py
@@ -6,18 +6,17 @@ WORLD_URL = "http://learncodethehardway.org/words.txt"
 WORDS = []
 
 PHRASES = {
-"class %%%(%%%):":
-"Make a class named %%% that is-a %%%.",
-"class %%%(object):\n\tdef __init__(self, ***)" :
-"class %%% has-a __init__ that takes self and *** params.",
-"class %%%(object):\n\tdef ***(self, @@@)":
-"class %%% has-a function *** that takes self and @@@ params.",
-"*** = %%%()":
-"Set *** to an instance of class %%%.",
-"***.***(@@@)":
-"From *** get the *** function, call it with params self, @@@.",
-"***.*** = '***'":
-"From *** get the *** attribute and set it to '***'."
+    "class %%%(%%%):": "Make a class named %%% that is-a %%%.",
+    "class %%%(object):\n\tdef __init__(self, ***)":
+    "class %%% has-a __init__ that takes self and *** params.",
+    "class %%%(object):\n\tdef ***(self, @@@)":
+    "class %%% has-a function *** that takes self and @@@ params.",
+    "*** = %%%()":
+    "Set *** to an instance of class %%%.",
+    "***.***(@@@)":
+    "From *** get the *** function, call it with params self, @@@.",
+    "***.*** = '***'":
+    "From *** get the *** attribute and set it to '***'."
 }
 
 
@@ -29,6 +28,7 @@ else:
 for word in urlopen(WORLD_URL).readlines():
     WORDS.append(str(word.strip(), encoding="utf-8"))
 
+
 def convert(snippet, phrase):
     class_names = [w.capitalize() for w in random.sample(WORDS, snippet.count("%%%"))]
     other_names = random.sample(WORDS, snippet.count("***"))
@@ -36,7 +36,7 @@ def convert(snippet, phrase):
     param_names = []
 
     for i in range(0, snippet.count("@@@")):
-        param_count = random.randint(1,3)
+        param_count = random.randint(1, 3)
         param_names.append(', '.join(random.sample(WORDS, param_count)))
 
     for sentence in snippet, phrase:
@@ -53,6 +53,8 @@ def convert(snippet, phrase):
 
         results.append(result)
     return results
+
+
 try:
     while True:
         snippets = list(PHRASES.keys())
